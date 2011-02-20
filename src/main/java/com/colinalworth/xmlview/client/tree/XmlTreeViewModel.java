f32fb940097030f486053e385240146a4ed657d5
Colin Alworth <niloc132@gmail.com>
2011-02-19 22:05:17 -0600

    Basic editable xml tree available

--- src/main/java/com/colinalworth/xmlview/client/tree/XmlTreeViewModel.java
+++ src/main/java/com/colinalworth/xmlview/client/tree/XmlTreeViewModel.java
@@ -19,12 +19,17 @@ package com.colinalworth.xmlview.client.tree;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.cell.client.AbstractEditableCell;
 import com.google.gwt.cell.client.Cell;
 import com.google.gwt.cell.client.CompositeCell;
 import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.cell.client.HasCell;
 import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.cell.client.ValueUpdater;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.safehtml.client.SafeHtmlTemplates;
+import com.google.gwt.safehtml.client.SafeHtmlTemplates.Template;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
@@ -109,9 +114,12 @@ public class XmlTreeViewModel implements TreeViewModel {
 	@Override
 	public boolean isLeaf(Object value) {
 		Node node = (Node)value;
-		if (node instanceof Element || node instanceof Document) {
+		if (node instanceof Document) {
+			//TODO consider replacing this with return true
+			return !node.hasChildNodes();
+		} else if (node instanceof Element) {
 			//is not a leaf if it has children, or if it has attributes
-			return !node.hasChildNodes()/* && !node.hasAttributes()*/;
+			return !node.hasChildNodes() && !node.hasAttributes();
 		}
 		//if not a Node, it is a leaf
 		return true;
@@ -120,31 +128,124 @@ public class XmlTreeViewModel implements TreeViewModel {
 	private List<Node> getChildren(Node node) {
 		List<Node> list = new ArrayList<Node>();
 
+		if (node instanceof Element) {
+			for (int i = 0; i < node.getAttributes().getLength(); i++) {
+				list.add(node.getAttributes().item(i));
+			}
+		}
 		for (int i = 0; i < node.getChildNodes().getLength(); i++) {
 			list.add(node.getChildNodes().item(i));
 		}
+
 		return list;
 	}
-	private class ElementCell extends AbstractCell<Node> {
+	private static class ViewState {
+		enum Section { TagName, AttributeName, AttributeValue, Content, None }
+		public Section section;
+		//public String sectionName;
+
+		public String value;
+	}
+	public static class ElementCell extends AbstractEditableCell<Node, ViewState> {
+		interface Template extends SafeHtmlTemplates {
+			@Template("<span>&lt;<input type=\"text\" value=\"{0}\" />&gt;</span>")
+			SafeHtml element(String nodeName);
+			@Template("<span><input type=\"text\" value=\"{0}\" /> = <input type=\"text\" value=\"{1}\" /></span>")
+			SafeHtml attribute(String name, String value);
+			@Template("<span>#text <textarea>{0}</textarea></span>")
+			SafeHtml text(String contents);
+			@Template("<span>#cdata <textarea>{0}</textarea></span>")
+			SafeHtml cdata(String contents);
+		}
+		private static Template template;
+		private Object lastKey;
+
+
+		public ElementCell() {
+			if (template == null) {
+				template = GWT.create(Template.class);
+			}
+		}
 		@Override
 		public void render(Cell.Context context, Node value, SafeHtmlBuilder sb) {
-			sb.appendHtmlConstant("<table><thead><tr><td>").appendEscaped("<" + value.getNodeName() + ">").appendHtmlConstant("</td></tr></thead>");
-			sb.appendHtmlConstant("<tbody><tr><td></td>");
-			if (value instanceof Element) {
-				for (int i = 0; i < value.getAttributes().getLength(); i++) {
-					sb.appendHtmlConstant("<td>");
-					renderAttr((Attr)value.getAttributes().item(i), sb);
-					sb.appendHtmlConstant("</td>");
-				}
-			} else if (value instanceof CharacterData || value instanceof Text) {
-				//this is probably wrong for cdata
-				sb.appendHtmlConstant("<td>").appendEscaped(value.getNodeValue()).appendHtmlConstant("</td>");
+			Object key = context.getKey();
+			ViewState viewData = getViewData(key);
+			if (viewData != null && viewData.section != ViewState.Section.None &&
+					viewData.value != null && viewDataMatchesValue(viewData, value)) {
+				clearViewData(key);
+				viewData = null;
 			}
-			sb.appendHtmlConstant("</tr></tbody></table>");
+
+			if (value instanceof Element) {
+				sb.append(template.element(value.getNodeName()));
+			} else if (value instanceof CharacterData) {
+				sb.append(template.cdata(value.getNodeValue()));
+			} else if (value instanceof Text) {
+				sb.append(template.text(value.getNodeValue()));
+			} else if (value instanceof Attr) {
+				sb.append(template.attribute(((Attr)value).getName(), value.getNodeValue()));
+			}
 		}
 
-		private void renderAttr(Attr item, SafeHtmlBuilder sb) {
-			sb.appendEscaped(item.getName()).appendEscaped("=").appendEscaped(item.getValue());
+
+		/**
+		 * @param viewData
+		 * @param value
+		 * @return
+		 */
+		private boolean viewDataMatchesValue(ViewState viewData, Node value) {
+			switch (viewData.section) {
+			case TagName:
+				assert value instanceof Element;
+				return viewData.value.equals(value.getNodeName());
+			case AttributeName:
+				assert value instanceof Attr;
+				return viewData.value.equals(((Attr)value).getName());
+			case AttributeValue:
+				assert value instanceof Attr;
+				return viewData.value.equals(value.getNodeValue());
+			case Content:
+				assert value instanceof CharacterData || value instanceof Text;
+				return viewData.value.equals(value.getNodeValue());
+			case None:
+			default:
+			}
+			return false;
+		}
+
+		@Override
+		public boolean isEditing(Cell.Context context, com.google.gwt.dom.client.Element parent, Node value) {
+			return lastKey != null && lastKey.equals(context.getKey());
+		}
+
+		@Override
+		public boolean resetFocus(Cell.Context context, com.google.gwt.dom.client.Element parent, Node value) {
+			if (isEditing(context, parent, value)) {
+				//TODO focus correct element
+				return true;
+			}
+
+			return false;
+		}
+
+		@Override
+		public void onBrowserEvent(Cell.Context context,
+				com.google.gwt.dom.client.Element parent, Node value,
+				NativeEvent event, ValueUpdater<Node> valueUpdater) {
+			super.onBrowserEvent(context, parent, value, event, valueUpdater);
+			com.google.gwt.dom.client.Element target = event.getEventTarget().cast();
+
+
+
+		}
+
+		@Override
+		protected void onEnterKeyDown(Cell.Context context,
+				com.google.gwt.dom.client.Element parent, Node value,
+				NativeEvent event, ValueUpdater<Node> valueUpdater) {
+			//if the event is directed at an input, finish editing, otherwise focus on first elt
+			com.google.gwt.dom.client.Element target = event.getEventTarget().cast();
+			//TODO
 		}
 	}
 }

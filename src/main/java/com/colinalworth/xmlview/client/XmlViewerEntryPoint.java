4b26c2e4d88442f089da703c2a553ff4f382f8d2
Colin Alworth <niloc132@gmail.com>
2011-02-23 12:35:12 -0600

    Made use of the generator in two simple examples, one for editing a maven pom, one for spring-drools configs. Note that the spring-drools stuff is commented out for now, as the url I had for the schema is no longer available.
    Stubbed in some code for a better demo, will be completed after refactoring out the rest of the project into a jar proj.

--- src/main/java/com/colinalworth/xmlview/client/XmlViewerEntryPoint.java
+++ src/main/java/com/colinalworth/xmlview/client/XmlViewerEntryPoint.java
@@ -17,8 +17,17 @@
 package com.colinalworth.xmlview.client;
 
 import com.colinalworth.xmlview.client.tree.XmlTreeViewModel;
+import com.colinalworth.xmlview.client.validator.XmlValidator;
+import com.colinalworth.xmlview.client.validator.XmlValidator.SchemaURL;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.cellview.client.CellTree;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.xml.client.Document;
 import com.google.gwt.xml.client.XMLParser;
@@ -28,32 +37,56 @@ import com.google.gwt.xml.client.XMLParser;
  *
  */
 public class XmlViewerEntryPoint implements EntryPoint {
-	private CellTree t;
+	@SchemaURL({"http://fisheye.jboss.org/browse/~raw,r=35924/JBossRules/trunk/drools-container/drools-spring/src/main/resources/org/drools/container/spring/drools-spring-1.2.0.xsd", "http://www.springframework.org/schema/beans/spring-beans-2.0.xsd"})
+	interface DroolsSpringSchema extends XmlValidator {}
+
+	@SchemaURL("http://maven.apache.org/xsd/maven-4.0.0.xsd")
+	interface MavenPom extends XmlValidator {}
+	interface Binder extends UiBinder<HTMLPanel, XmlViewerEntryPoint> {}
+
+	private Binder binder = GWT.create(Binder.class);
+	@UiField(provided = true) CellTree tree;
 
 	@Override
 	public void onModuleLoad() {
+		Document rootValue = XMLParser.parse("<beans xmlns='http://www.springframework.org/schema/beans' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'	xmlns:drools='http://drools.org/schema/drools-spring'	xsi:schemaLocation='http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans-2.0.xsd                        http://drools.org/schema/drools-spring org/drools/container/spring/drools-spring-1.2.0.xsd'> 	<bean id='ds' class='org.springframework.jdbc.datasource.DriverManagerDataSource'> 		<!-- org.h2.jdbcx.JdbcDataSource --> 		<property name='driverClassName' value='org.h2.Driver' /> 			<property name='url' value='jdbc:h2:tcp://localhost/DroolsFlow' /> 		<property name='username' value='sa' /> 		<property name='password' value='' /> 	</bean> 	<drools:grid-node id='node1' /> <drools:kbase id='kbProcessWorkItems' node='node1'><drools:resources> <drools:resource type='PKG' source='file:///${temp.dir}/processWorkItems.pkg' /> <drools:resource type='PKG' source='file:///${temp.dir}/processSubProcess.pkg' /> <drools:resource type='PKG' source='file:///${temp.dir}/processTimer.pkg' /> <drools:resource type='PKG' source='file:///${temp.dir}/processTimer2.pkg' /> </drools:resources> </drools:kbase></beans>");
+
+		// passing in the full document, as the children of the first element are parsed, not the root itself
+		//DroolsSpringSchema schema = GWT.create(DroolsSpringSchema.class);
+
+		MavenPom pom = GWT.create(MavenPom.class);
+
 		XmlTreeViewModel viewModel = new XmlTreeViewModel();
-
-
-		Document rootValue = XMLParser.parse("<zones><zone name='Bedrooms' id='6'><sensor id='1' name='Hallway'>string content!</sensor><sensor id='2' name='Master'></sensor></zone><zone name='Living Area' id='8'><sensor id='3' /></zone><zone name='Garage' id='9'><sensor id='4' /></zone><zone name='Basement' id='10'><sensor id='5' /></zone></zones>");
-
-		t = new CellTree(viewModel, rootValue);
-		RootPanel.get().add(t);
+		tree = new CellTree(viewModel, rootValue);
+		RootPanel.get().add(binder.createAndBindUi(this));
 
 
 	}
 
-	public void validate() {
-		Document d = (Document)t.getRootTreeNode().getValue();
+	public void load() {
+
+	}
+
+	public void switchToText() {
+
+	}
+	public void switchToTree() {
+
+	}
+
+	@UiHandler("validate")
+	public void validate(ClickEvent evt) {
+		Document d = (Document)tree.getRootTreeNode().getValue();
 
 
 		//pass to server
-		d.toString();
+		Window.alert(d.toString());
 
 
 	}
-	public void save() {
-		Document d = (Document)t.getRootTreeNode().getValue();
+	@UiHandler("save")
+	public void save(ClickEvent evt) {
+		Document d = (Document)tree.getRootTreeNode().getValue();
 
 
 		//pass to server
